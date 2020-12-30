f5acfd5d943ea8f684d30453b1081bf2b86978fd
Vladimir Lewandowski <vova.space@icloud.com>
2020-12-30 16:45:36 +0300

    Add: Memoji animation

--- src/components/Memoji/Memoji.tsx
+++ src/components/Memoji/Memoji.tsx
@@ -1,5 +1,5 @@
 import { FunctionComponent } from 'preact';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 import { rem } from '@styled';
 
@@ -14,9 +14,24 @@ export interface MemojiProps {
   className?: string;
 }
 
+const animation = keyframes`
+  0%  {
+    transform: scale(1);
+  }
+
+  50% {
+    transform: scale(1.04);
+  }
+
+  100% {
+    transform: scale(1);
+  }
+`;
+
 const StyledPicture = styled.picture`
   display: flex;
   flex-shrink: 0;
+  animation: ${animation} 2s linear infinite;
 `;
 
 const StyledImage = styled.img`
@@ -28,6 +43,6 @@ export const Memoji: FunctionComponent<MemojiProps> = ({ className }) => (
   <StyledPicture className={className}>
     <source srcSet={`${imageWEBP}, ${imageWEBPx2} 2x, ${imageWEBPx3} 3x`} type="image/webp" />
     <source srcSet={`${imagePNG}, ${imagePNGx2} 2x, ${imagePNGx3} 3x`} type="image/png" />
-    <StyledImage src={imagePNGx3} width="69" height="80" alt="Memoji" role="presentation" />
+    <StyledImage src={imagePNGx2} width="69" height="80" alt="Memoji" role="presentation" />
   </StyledPicture>
 );
